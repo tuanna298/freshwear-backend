dd4a1f72f4340fc0c152b7835b2f3a9bc0b57fc2
tuanna29 <tuanna5@medicskin.vn>
2024-11-23 15:38:22 +0700

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './shared/prisma/prisma.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
